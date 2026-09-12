3e0894af425edf65a5eb51a83f80c9be764fc179
Grant <grobert2137@gmail.com>
2026-09-12 12:32:31 -0500

    In progress: lab-1 prelab

---
+++ lab_1/sort.cpp
@@ -0,0 +1,39 @@
+#include <cstdio> // for printf and scanf
+
+int main() 
+{
+    int numbers[10] = {9, 4, 5, 2, 3, 0, 8, 1, 7, 6};
+    int val;
+
+    printf("Here's the array that will be sorted:");
+
+    // loop thru all values in numbers array
+    for (int m = 0; m < 10; m++)
+    {
+        printf("%d ", numbers[m]);
+    }
+
+    printf("\nThese will now be sorted...");
+
+    for (int i = 0; i < 10; i++) 
+    {
+        for (int j = 1; j < i; j++) 
+        {
+            if (numbers[j] < numbers[i]) {
+                val = numbers[i];
+                numbers[i] = numbers[j];
+                numbers[j] = val;
+
+            }
+        }
+    }
+
+    printf("\nHere's the sorted array:");
+
+    for (int n = 0; n < 10; n++)
+    {
+        printf("%d ", numbers[n]); //added a space after %d so each number would seperate
+    }
+}
+
+
---
+++ lab_1/temperature.cpp
@@ -0,0 +1,22 @@
+// iostream library is C++ only. Its only needed if you do std::cout or std::cin
+// BUT iostream also drags in cstdio as a its own header internally.
+#include <iostream>
+
+// Explicitly declare cstdio for good practice when using printf() and scanf_s() <------------- 
+#include <cstdio>
+
+//Declare vars
+float temp_c;
+float temp_f;
+
+float convert_to_f(float c) {
+    float f = (9.0f/5.0f)*c + 32;
+    return f;
+}
+
+int main() {
+    printf("\nHi! Please enter a temperature in degrees Celcius:");
+    scanf_s("%f", &temp_c);
+    temp_f = convert_to_f(temp_c);
+    printf("\nTemperature in F: %f", temp_f);
+}
