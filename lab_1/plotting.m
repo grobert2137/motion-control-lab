7cd3ac3cb75ef8fd9f066b8adb5460cff2d65a4f
Grant Mac <grobert2137@gmail.com>
2026-09-14 13:15:19 -0500

    updated plotting matlab script

--- lab_1/plotting.m
+++ lab_1/plotting.m
@@ -1,4 +1,21 @@
-t = 0:0.1:5;
-y = sin(t);
+clear; close all;
 
-plot(t,y);
+% create time vector
+t = 0:0.1:5;
+
+% create amplitudes
+a1 = 1;
+a2 = 0.5;
+
+% create frequencies
+f1 = 1;
+f2 = 10; 
+
+y1 = a1*cos(2*pi*t*f1);
+
+y2 = a2*cos(2*pi*t*f2);
+
+plot(t,y1);
+grid on; hold on;
+plot(t, y2);
+legend on;
